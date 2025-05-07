b25601d4cfaba3016b62c46c56ca4829fce14938
santosh sanap <santoshsanap143145@gmail.com>
2025-05-07 12:46:57 +0530

    tdForm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { countries } from './const/countries';
+import { states } from './const/states';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tdForm';
+
+  validators = {
+    password:
+      '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[$@$!%*?&])[A-Za-z\\d$@$!%*?&]{8,}$',
+    onlyText: '^[a-zA-Z]*$',
+    username: '^[a-zA-Z0-9_]+$',
+    email: '^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$',
+    pincode: '^[1-9][0-9]{5}$',
+    phoneNo: '^(+91[-s]?)?[6-9][0-9]{9}$',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
